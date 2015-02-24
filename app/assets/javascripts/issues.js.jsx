f06f1ba2af66591a5e9655f072c5b6f5d18d1610
Shim Won <marocchino@gmail.com>
2015-02-24 22:59:49 +0900

    Add drag UI

--- app/assets/javascripts/issues.js.jsx
+++ app/assets/javascripts/issues.js.jsx
@@ -1,7 +1,12 @@
+var placeholder = document.createElement("li");
+placeholder.className = "placeholder panel";
+placeholder.innerText = "Drop here"
+
 var KanbanBox = React.createClass({
   getInitialState: function() {
     return {data: []};
   },
+  // api call
   getIssues: function() {
     $.ajax({
       url: this.props.url,
@@ -13,6 +18,62 @@ var KanbanBox = React.createClass({
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
+  },
+  setStatusOfIssue: function() {
+    console.log("setStatusOfIssue:");
+    console.log("  id: " + this.dragged.dataset.id);
+    console.log("  status: " + this.over.parentNode.dataset.status);
+  },
+  reorderIssue: function(){
+    console.log("reorderIssue:");
+    console.log("  id: " + this.dragged.dataset.id);
+    console.log("  targetId: " + this.over.dataset.id);
+    console.log("  placement: " + this.nodePlacement);
+  },
+  // utility
+  setNodePlacement: function(e) {
+    var relY = e.clientY - this.over.offsetTop;
+    var height = this.over.offsetHeight / 2;
+    if(relY > height) {
+      this.nodePlacement = "after";
+    } else if(relY < height) {
+      this.nodePlacement = "before";
+    }
+  },
+  insertPlaceholder: function() {
+    var parent = this.over.parentNode;
+    if(this.nodePlacement == "before") {
+      parent.insertBefore(placeholder, this.over.nextElementSibling);
+    } else {
+      parent.insertBefore(placeholder, this.over);
+    }
+  },
+  // event
+  dragStart: function(e) {
+    this.dragged = e.currentTarget;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData("text/html", e.currentTarget);
+  },
+  dragEnd: function(e) {
+    this.dragged.style.display = "block";
+    var targetList = this.over.parentNode;
+    var originList = this.dragged.parentNode;
+    targetList.removeChild(placeholder);
+
+    // Update state here
+    if (targetList != originList) {
+      this.setStatusOfIssue();
+    }
+    this.reorderIssue();
+  },
+  dragOver: function(e) {
+    e.preventDefault();
+    this.dragged.style.display = "none";
+    if(e.target.tagName != "LI" ||
+       e.target.classList.contains("placeholder")) return;
+    this.over = e.target;
+    this.setNodePlacement(e);
+    this.insertPlaceholder();
   },
   componentDidMount: function() {
     this.getIssues();
@@ -43,25 +104,24 @@ var IssueList = React.createClass({
     var statusFilter = function (issue) {
       return status == issue.status;
     };
-    var issueNodes = this.props.data.filter(statusFilter).map(function (issue) {
+    var issueNodes = this.props.data.filter(statusFilter).map((function (issue) {
       return (
-        <Issue key={issue.id} object={issue} />
+        <li data-id={issue.id}
+            key={issue.id}
+            draggable="true"
+            onDragEnd={this._owner.dragEnd}
+            onDragStart={this._owner.dragStart}
+            className="panel callout radius">
+          {issue.title}
+        </li>
       );
-    });
+    }).bind(this));
     return (
-      <div className="panel">
+      <ul className="panel issues"
+          data-status={status}
+          onDragOver={this._owner.dragOver}>
         {issueNodes}
-      </div>
-    );
-  }
-});
-
-var Issue = React.createClass({
-  render: function() {
-    return (
-      <div className="panel callout radius">
-        {this.props.object.title}
-      </div>
+      </ul>
     );
   }
 });
@@ -70,11 +130,12 @@ var IssueForm = React.createClass({
   render: function() {
     return (
       <div className="issueForm">
-        Hello, world! I am a IssueForm.
+        Hello! I am a IssueForm.
       </div>
     );
   }
 });
+
 React.render(
   <KanbanBox url='/api/issues' />,
   document.getElementById('kanban')
