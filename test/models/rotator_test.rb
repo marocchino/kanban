2a421a213441c5f0711c78620b9399efdd411a06
Shim Won <marocchino@gmail.com>
2016-08-20 04:29:21 +0900

    Make helper method to private

--- test/models/rotator_test.rb
+++ test/models/rotator_test.rb
@@ -2,14 +2,6 @@
 require 'test_helper'
 
 class RotatorTest < ActiveSupport::TestCase
-  def rotate(current, target, placement)
-    issues = (1..4).map { |i| Issue.new priority: i }
-    issue = Issue.new priority: current, status: 'todo'
-    Issue.stub :ordered_status, issues do
-      Rotator.new(issue, target, placement).rotated_priorities
-    end
-  end
-
   def test_rotated_priorities_before
     assert_equal([1, 2, 4, 3], rotate(4, 3, 'before'))
     assert_equal([1, 3, 4, 2], rotate(4, 2, 'before'))
@@ -44,5 +36,15 @@ class RotatorTest < ActiveSupport::TestCase
     assert_equal([4, 1, 2, 3], rotate(1, 4, 'after'))
     assert_equal([3, 1, 2, 4], rotate(1, 3, 'after'))
     assert_equal([2, 1, 3, 4], rotate(1, 2, 'after'))
+  end
+
+  private
+
+  def rotate(current, target, placement)
+    issues = (1..4).map { |i| Issue.new(priority: i) }
+    issue = Issue.new(priority: current, status: 'todo')
+    Issue.stub(:ordered_status, issues) do
+      Rotator.new(issue, target, placement).rotated_priorities
+    end
   end
 end
