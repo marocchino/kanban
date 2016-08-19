f8eabf8e8e2b9395498e168b31986e3d6451d3e2
Shim Won <marocchino@gmail.com>
2016-08-20 03:53:04 +0900

    Remove deprecate warnings

--- test/controllers/api/issues_controller_test.rb
+++ test/controllers/api/issues_controller_test.rb
@@ -2,9 +2,9 @@
 require 'test_helper'
 
 module Api
-  class IssuesControllerTest < ActionController::TestCase
+  class IssuesControllerTest < ActionDispatch::IntegrationTest
     def test_index
-      get :index
+      get '/api/issues'
       hash = JSON.parse(response.body)
       assert_kind_of Array, hash['data']
       assert_response :success
@@ -12,7 +12,7 @@ module Api
 
     def test_update_without_target
       issue = issues(:one)
-      put :update, id: issue.id, status: 'done'
+      put "/api/issues/#{issue.id}", params: { status: 'done' }
       issue.reload
       assert_equal 'done', issue.status
       assert_response :success
@@ -20,11 +20,8 @@ module Api
 
     def test_update_with_target
       issue = issues(:one)
-      put :update,
-          id: issue.id,
-          status: 'done',
-          target_priority: '1',
-          placement: 'after'
+      put "/api/issues/#{issue.id}",
+          params: { status: 'done', target_priority: '1', placement: 'after' }
       issue.reload
       assert_equal 'done', issue.status
       assert_response :success
