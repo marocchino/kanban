68cb50b02aff3dfc1f313603a430f08071889ed8
Shim Won <marocchino@gmail.com>
2016-08-20 03:47:31 +0900

    New default

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# encoding: UTF-8
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_150_223_093_940) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 20150223093940) do
 
-  create_table 'issues', force: :cascade do |t|
-    t.string   'title'
-    t.integer  'priority',   default: 5
-    t.integer  'status',     default: 0
-    t.datetime 'created_at',             null: false
-    t.datetime 'updated_at',             null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "issues", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "priority",   default: 5
+    t.integer  "status",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
+
 end
